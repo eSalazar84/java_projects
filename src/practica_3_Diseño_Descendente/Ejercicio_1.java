c795bc5a59249738b9d3b59725029fd614191d9a
eSalazar84 <salazaremiliano84@gmail.com>
2025-04-23 10:14:20 -0300

    first commit

---
+++ src/practica_3_Diseño_Descendente/Ejercicio_1.java
@@ -0,0 +1,56 @@
+package practica_3_Diseño_Descendente;
+
+import utils.*;
+
+/*  
+ * Cuando se pide un diseño es hacer el pseudocódigo. En
+todos los casos resolver haciendo uso del diseño
+descendente.
+1. Escribir un diseño de programa que mientras que el usuario
+ingrese un número distinto de 0, pida ingresar otro número y lo
+imprima.
+
+ * Tentativa de resolucion
+ 1- Pedir al usuario que ingrese un numero
+ 2- Mientras que el numero ingresado sea distinto de cero, repetir paso 1
+ 2-i- Imprimir por pantalla el numero que ingreso el usuario
+ 3- En caso de ser cero, terminar el programa
+ */
+
+// Tentativa sin Diseño Descendente
+/* public class Ejercicio_1 {
+    public static void main(String[] args) {
+        int numero = 0;
+        System.out.println("Ejercicio 1");
+        System.out.println("Ingrese un numero natural: ");
+        numero = Utils.leerInt();
+        while (numero != 0) {
+            System.out.println("El numero que ingresó es el " + numero);
+            System.out.println("Ingrese otro numero natural: ");
+            numero = Utils.leerInt();
+        }
+        System.out.println("Usted a ingresado el numero cero " + numero + " Gracias por usar nuestro programa");
+    }
+} */
+
+// Tentativa con Diseño Descendente
+
+public class Ejercicio_1 {
+    public static int numero = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Ejercicio 1");
+        numero = numeroIngresado(numero);
+        while (numero != 0) {
+            System.out.println("El numero que ingresó es el " + numero);
+            numero = numeroIngresado(numero);
+        }
+        System.out.println("Usted a ingresado el numero cero (" + numero + "), Gracias por usar nuestro programa");
+    }
+
+    public static int numeroIngresado(int numeroUsuario) {
+        System.out.println("Ingrese un numero natural: ");
+        numero = Utils.leerInt();
+        return numero;
+    }
+}
---
+++ src/practica_3_Diseño_Descendente/Ejercicio_2.java
@@ -0,0 +1,16 @@
+package practica_3_Diseño_Descendente;
+
+import utils.*;
+
+/* 
+ * 2. Escribir un diseño de programa que mientras que el usuario
+ingrese un carácter dígito o carácter letra minúscula, imprima
+dicho carácter, y si es carácter letra minúscula, imprima si es vocal
+o consonante.
+ */
+
+public class Ejercicio_2 {
+   public static void main(String[] args) {
+    
+   } 
+}
